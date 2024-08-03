33fe8a9c532e70552e6fd2528f4023c04f636b54
Michael T. Mordowanec <5489487+Mordeaux@users.noreply.github.com>
2024-08-03 02:17:19 -0400

    Add time to history page

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -17,6 +17,55 @@ const Die = ({ dieSide, color }: { dieSide: number; color: DieColor }) => (
   />
 );
 
+const formatDateTime = (date: Date) => {
+  const getOrdinal = (n: number) => {
+    const finalDigit = n % 10;
+    switch (finalDigit) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  };
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  const hour = date.getHours().toString().padStart(2, "0");
+  const minute = date.getMinutes().toString().padStart(2, "0");
+  const second = date.getSeconds().toString().padStart(2, "0");
+
+  return `On ${dayOfWeek}, the ${day}${getOrdinal(day)} of ${month}, ${year} at ${hour}:${minute}:${second}`;
+};
+
 const GameHistory = ({ game }: { game: GameHistoryType[] }) => {
   let previousBalance;
   const children = game.map((gameState, i) => {
@@ -34,6 +83,7 @@ const GameHistory = ({ game }: { game: GameHistoryType[] }) => {
       return (
         <div key={i}>
           <p>
+            {formatDateTime(gameState.createdAt)}:<br />
             You bet {gameState.betAmount} chip
             {gameState.betAmount > 1 ? "s" : ""} on:{" "}
             <Die dieSide={gameState.betValue} color={DieColor.Black} />
